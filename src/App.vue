581f1a2f49e25def99f072b69cab40e383bbf728
YSoli <yann.soliveres@gmail.com>
2021-09-06 13:00:02 +0200

    basis

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <div v-for="message in messages" :key="message.title">
+      <p>{{message.title}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import messages from './assets/postit.js'
+
+export default {
+  name: 'App',
+  data:()=>{
+    return {
+      messages: messages.messages
+    }
+  },
+  components: {
+  },
+  created(){
+    
+  }
+}
+</script>
+
+<style>
+
+@import 'assets/scss/style.scss';
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+
+</style>
---
+++ src/assets/postit.js
@@ -0,0 +1,34 @@
+export default
+{"messages":[
+        {
+            "title":"Hello",
+            "text":"Lorem ipsum et caetera quidam",
+            "author":"Bob",
+            "date":"10/10/2021"
+        },
+        {
+            "title":"Hello 2",
+            "text":"Lorem ipsum et caetera quidam",
+            "author":"Bob",
+            "date":"10/10/2021"
+        },
+        {
+            "title":"Hello 3",
+            "text":"Lorem ipsum et caetera quidam",
+            "author":"Bob",
+            "date":"10/10/2021"
+        },
+        {
+            "title":"Hello 4",
+            "text":"Lorem ipsum et caetera quidam",
+            "author":"Bob",
+            "date":"10/10/2021"
+        },
+        {
+            "title":"Hello 5",
+            "text":"Lorem ipsum et caetera quidam",
+            "author":"Bob",
+            "date":"10/10/2021"
+        }
+    ]
+}
